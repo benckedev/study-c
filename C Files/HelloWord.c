9a5ea5d3deb19c3fc07b88106239bdd754193dc2
yooout <youtpigs@gmail.com>
2023-02-20 20:57:53 -0300

    🟢 Constant

--- C Files/HelloWord.c
+++ C Files/HelloWord.c
@@ -3,25 +3,11 @@
 int main()
 {
 
-    // format specifier %
+    // constant = fixed value that cannot be altered by the program during its execution
 
-    // %c = char
-    // %s = string || char[]
-    // %f = float
-    // %lf = double
-    // %d = integer
+    const float PI = 3.14159; // uppercase = good practice
 
-    // %.1 = decimal precision
-    // %1 = minimum field width
-    // %- = left align
-
-    float item1 = 5.75;
-    float item2 = 10.00;
-    float item3 = 100.99;
-
-    printf("Item 1: $%.2f", item1);
-    printf("\nItem 2: $%.2f", item2);
-    printf("\nItem 3: $%.2f", item3);
+    printf("%f", PI);
 
     return 0;
 }
