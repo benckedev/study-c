346ff87fa9541cf98a9ce84ee979cf3336b79db9
yooout <youtpigs@gmail.com>
2023-02-20 20:18:46 -0300

    🟢 Escape sequence

--- C Files/HelloWord.c
+++ C Files/HelloWord.c
@@ -3,14 +3,12 @@
 int main()
 {
 
-    // This is a comment
-    /*
-        This
-        is
-        a
-        multiline
-        comment
-    */
-    printf("Hello World!");
+    // \n = newline - create a newline in the message ex.: Hello\nWorld!
+    // \t = tab - create a TAB space between the letters ex.: He\tl\tlo World!
+    // \ = can print " or ' in a string ex.: \"Hello\" World!
+
+    printf("Hello\nWorld!\n");
+    printf("Hell\to\tWorld!\n");
+    printf("\"Hello\" World!");
     return 0;
 }
