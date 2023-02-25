a8fb680858040cafe448eb18bc5a43e0526a1722
yooout <youtpigs@gmail.com>
2023-02-25 20:21:52 -0300

    ⭐ Circle area and circumference calculator

--- C Files/HelloWord.c
+++ C Files/HelloWord.c
@@ -4,18 +4,19 @@
 int main()
 {
 
-    double A = sqrt(9);    // √9
-    double B = pow(2, 4);  // 4^2
-    int C = round(3.14);   // 3.14 = 3
-    int D = ceil(3.14);    // 3.14 = 4
-    int E = floor(3.14);   // 3.14 = 3
-    double F = fabs(-100); // make positive
-    double G = log(3);     // logarithm
-    double H = sin(45);
-    double I = cas(45);
-    double J = tan(45);
+    const double PI = 3.14159;
+    double radius;
+    double circumference;
+    double area;
 
-    printf("\n%lf", E);
+    printf("\nEnter the radius of a circle\n>> ");
+    scanf("%lf", &radius);
+
+    circumference = 2 * PI * radius;
+    area = PI * radius * radius;
+
+    printf("\nCircumference: %.2lf", circumference);
+    printf("\nArea: %.2lf", area);
 
     return 0;
 }
