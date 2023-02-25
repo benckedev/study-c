8e8b6593e744be7888ec3c61b13bceb8eee486cd
yooout <youtpigs@gmail.com>
2023-02-25 20:03:54 -0300

    🟢 Augmented assignment operatos

--- C Files/HelloWord.c
+++ C Files/HelloWord.c
@@ -3,27 +3,29 @@
 int main()
 {
 
-    // arithmetic operators
+    // augmented assignment operatos =
+    // used to replace a statement where an operator
+    // takes a variable as onde of its arguments
+    // and then assigns the result back to the same variable
+    // x = x +1/
+    // x+=1;
 
-    // + (addition)
-    // - (subtraction)
-    // * (multiplication)
-    // / (division)
-    // % (modulus)
-    // ++ (increment)
-    // -- (decrement)
+    int x = 10;
 
-    int x = 5;
-    int y = 2;
+    // x = x + 2;
+    // x+= 2;
 
-    // int z = x + y;
-    // int z = x - y;
-    //  int z = x * y;
-    //  float z = x / (float)y;
-    // int z = x % y;
+    // x = x - 3;
+    // x-+3;
 
-    x++;
-    y--;
+    // x = x * 4;
+    // x*= 4;
+
+    // x = x / 5;
+    // x/= 5;
+
+    // x = x % 2;
+    // x%= 2;
 
     printf("%d", x);
 
